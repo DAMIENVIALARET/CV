646928de13e0849b13d598f1efb5ff364116292a
DAMIENVIALARET <vialaretdamien@gmail.com>
2024-11-02 14:28:21 -0400

    sc

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -29,13 +29,8 @@ const Landing: React.FC = () => {
               href="CV_Damien Adams Vialaret.pdf"
               className="text-2xl mx-2 text-gray-600 hover:text-gray-600"
             >
-              <img
-                src="https://cdn-icons-png.flaticon.com/128/8843/8843096.png"
-                alt="CV Icon"
-                className="h-6 w-6 mr-2 hover:scale-110"
-              />
+              <i className="fas fa-file-pdf hover:scale-110"></i>
             </a>
-
             <a
               href="mailto:vialaretdamien@gmail.com"
               target="_blank"
